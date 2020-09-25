11eb67e47e88ae93c56890398a2335d97a83c079
Adam Alfiansyah <adamalfiansyah@gmail.com>
2020-09-25 23:47:06 +0700

    Membuat table untuk jadwal sholat

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ function prayerTimes(latitude, longitude){
     .then(response => response.json())
     .then(function(response){
         let date        = new Date();
-        let today       = date.getDate() - 1;
+        let today       = date.getDate();
+        let data       = response.data[0].timings;
 
-        // console.log(response.data[today]);
+        let app         = document.getElementById('app');
+        let table       = document.createElement('table');
+        let tableTbody  = document.createElement('tbody');
+        
+        for(i in data){
+            let row         = tableTbody.insertRow();
+            let name        = row.insertCell(0);
+            let time        = row.insertCell(1);
+            name.innerHTML  = i;
+            time.innerHTML  = data[i];
+            tableTbody.appendChild(row);
+        }
+        
+        table.appendChild(tableTbody);
+        app.appendChild(table);
+
     });
 }
 
