a457cbd854bc109d5f33ccf42c5b00db81ecf63c
Adam Alfiansyah <adamalfiansyah@gmail.com>
2020-09-25 19:24:32 +0700

    permission geolocation

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+function success(position){
+    console.log(position);
+}
+
+function error(){
+    alert('Posisi tidak dapat di akses');
+}
+
+function userLocation(){
+    if(!navigator.geolocation){
+        alert('Geolocation tidak di dukung didalam browser anda, silahkan gunakan browser lain');
+    }else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index(){
     let app         = document.getElementById('app');
     let h3          = document.createElement('h3');
     h3.innerHTML    = 'Prayer Times';
     
     app.appendChild(h3);
+
+    userLocation();
 }
 
 index();
